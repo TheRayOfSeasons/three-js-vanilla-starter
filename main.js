62248c9253997cde97da1ee330d76fd461e2ee33
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2022-02-28 23:46:43 +0800

    Add: sample wave shader

--- main.js
+++ main.js
@@ -29,7 +29,32 @@ camera.position.z = 3;
 // mesh
 const geometry = new THREE.PlaneBufferGeometry(10, 10, 100, 100);
 geometry.rotateX(Math.PI * 0.5);
+// const material = new THREE.RawShaderMaterial({
+//   vertexShader: `
+//     attribute vec3 position;
+
+//     uniform mat4 projectionMatrix;
+//     uniform mat4 viewMatrix;
+//     uniform mat4 modelMatrix;
+
+//     void main()
+//     {
+//       gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+//     }
+//   `,
+//   fragmentShader: `
+//     precision mediump float;
+
+//     void main()
+//     {
+//       gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+//     }
+//   `
+// });
 const material = new THREE.RawShaderMaterial({
+  uniforms: {
+    uTime: { value: 0 }
+  },
   vertexShader: `
     attribute vec3 position;
 
@@ -37,19 +62,34 @@ const material = new THREE.RawShaderMaterial({
     uniform mat4 viewMatrix;
     uniform mat4 modelMatrix;
 
+    uniform float uTime;
+
+    varying vec4 vNewPosition;
+
     void main()
     {
-      gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+      vec4 newPosition = vec4(position, 1.0);
+      newPosition.y = sin((uTime * 0.001) + newPosition.x);
+      vNewPosition = newPosition;
+      gl_Position = projectionMatrix * viewMatrix * modelMatrix * newPosition;
     }
   `,
   fragmentShader: `
     precision mediump float;
 
+    varying vec4 vNewPosition;
+
+    float invert(float value)
+    {
+      return 1.0 - value;
+    }
+
     void main()
     {
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      gl_FragColor = vec4(0.2, vNewPosition.y, 5.0, 1.0);
     }
-  `
+  `,
+  side: THREE.DoubleSide,
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);

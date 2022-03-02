153d065377910e2b7f27a57b2fd2428753f00988
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2022-03-02 21:31:55 +0800

    Add: sound test

--- main.js
+++ main.js
@@ -24,7 +24,24 @@ const camera = new THREE.PerspectiveCamera(
   75,
   canvasWidth / canvasHeight
 );
-camera.position.z = 3;
+camera.position.z = 10;
+const listener = new THREE.AudioListener();
+camera.add(listener);
+scene.add(camera);
+
+const sound = new THREE.PositionalAudio(listener);
+sound.position.set(10, 0, 0);
+scene.add(sound);
+
+const audioLoader = new THREE.AudioLoader();
+
+audioLoader.load('sound.mp3', buffer => {
+  sound.setBuffer(buffer);
+	sound.setLoop(true);
+	sound.setVolume(0.5);
+  sound.setRefDistance(5);
+	sound.play();
+});
 
 // mesh
 const geometry = new THREE.PlaneBufferGeometry(10, 10, 100, 100);
@@ -57,6 +74,7 @@ const material = new THREE.RawShaderMaterial({
   },
   vertexShader: `
     attribute vec3 position;
+    attribute vec2 uv;
 
     uniform mat4 projectionMatrix;
     uniform mat4 viewMatrix;
@@ -65,12 +83,14 @@ const material = new THREE.RawShaderMaterial({
     uniform float uTime;
 
     varying vec4 vNewPosition;
+    varying vec2 vUv;
 
     void main()
     {
       vec4 newPosition = vec4(position, 1.0);
       newPosition.y = sin((uTime * 0.001) + newPosition.x);
       vNewPosition = newPosition;
+      vUv = uv;
       gl_Position = projectionMatrix * viewMatrix * modelMatrix * newPosition;
     }
   `,
@@ -78,6 +98,7 @@ const material = new THREE.RawShaderMaterial({
     precision mediump float;
 
     varying vec4 vNewPosition;
+    varying vec2 vUv;
 
     float invert(float value)
     {
@@ -86,7 +107,7 @@ const material = new THREE.RawShaderMaterial({
 
     void main()
     {
-      gl_FragColor = vec4(0.2, vNewPosition.y, 5.0, 1.0);
+      gl_FragColor = vec4(smoothstep(0.1, 0.9, vUv.x), vNewPosition.y + vUv.y, 5.0, 1.0);
     }
   `,
   side: THREE.DoubleSide,

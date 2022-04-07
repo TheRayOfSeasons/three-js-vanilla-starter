ef48cf9fd6876db4b8811a4d998373b9579c2075
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2022-04-07 19:20:45 +0800

    Add: pickup cubes logic

--- main.js
+++ main.js
@@ -24,10 +24,10 @@ const camera = new THREE.PerspectiveCamera(
   75,
   canvasWidth / canvasHeight
 );
-camera.position.z = 10;
+camera.position.z = 15;
 
 const light = new THREE.DirectionalLight('#e7e7e7');
-light.position.set(20, 100, -50);
+light.position.set(20, 100, 50);
 scene.add(light);
 
 const world = new CANNON.World();
@@ -36,8 +36,11 @@ world.gravity.set(0, -9.82, 0);
 // mesh
 const createBox = (x, y, z) => {
   const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
-  const material = new THREE.MeshNormalMaterial();
+  const material = new THREE.MeshPhongMaterial({
+    color: '#08acf9'
+  });
   const mesh = new THREE.Mesh(geometry, material);
+  mesh.name = 'Box';
   mesh.position.set(x, y, z);
 
   const shape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5));
@@ -47,6 +50,7 @@ const createBox = (x, y, z) => {
   body.position.x = mesh.position.x;
   body.position.y = mesh.position.y;
   body.position.z = mesh.position.z;
+  mesh.body = body;
 
   const update = () => {
     mesh.position.x = body.position.x;
@@ -105,24 +109,72 @@ const boxes = [
 ]
 
 const plane = createPlane();
+plane.mesh.isGround = true;
 
 // controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
 
 // clock
 const clock = new THREE.Clock();
 let delta = 0;
 
+// raycasting
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
+const raycastPlane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
+
+const getPlaneDirection = () => {
+  return new THREE.Vector3().copy(camera.position).sub(new THREE.Vector3(0, 0, 0));
+}
+
+raycastPlane.normal.copy(getPlaneDirection());
+
+window.addEventListener('pointermove', event => {
+  pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+});
+
+let SELECTED;
+
+window.addEventListener('click', event => {
+  const intersects = raycaster.intersectObjects(scene.children);
+  if (SELECTED) {
+    SELECTED.body.mass = 1;
+    SELECTED = null;
+    return;
+  }
+  if (intersects[0]?.object) {
+    SELECTED = intersects[0].object;
+  }
+});
+
 // render
 renderer.setAnimationLoop(time => {
   delta = Math.min(clock.getDelta(), 0.1)
   world.step(delta);
+
+  raycastPlane.normal.copy(getPlaneDirection());
+
+  const target = new THREE.Vector3();
+  raycaster.setFromCamera(pointer, camera);
+  raycaster.ray.intersectPlane(raycastPlane, target);
+
+  if (SELECTED) {
+    if (SELECTED?.body) {
+      SELECTED.body.mass = 0;
+      SELECTED.body.position.x = target.x;
+      SELECTED.body.position.y = target.y;
+      SELECTED.body.position.z = target.z;
+    }
+  }
+
   for (const box of boxes) {
     box.update();
   }
   plane.update();
-  controls.update();
+  // controls.update();
   renderer.render(scene, camera);
 });
 

76636328f824316943077cb94c7d3c2e3a86ed6e
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2022-04-21 18:49:42 +0800

    Add: basic top down shooter movement

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const camera = new THREE.PerspectiveCamera(
   75,
   canvasWidth / canvasHeight
 );
-camera.position.z = 3;
+camera.position.z = 10;
 
 // mesh
 const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
@@ -31,9 +31,76 @@ const material = new THREE.MeshNormalMaterial();
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+const front = new THREE.Object3D();
+front.position.y = 1;
+const frontHelper = new THREE.AxesHelper(0.5);
+front.add(frontHelper);
+mesh.add(front);
+
+const back = new THREE.Object3D();
+back.position.y = -1;
+const backHelper = new THREE.AxesHelper(0.5);
+back.add(backHelper);
+mesh.add(back);
+
+const bullets = [];
+const Pressed = {
+  W: false,
+  A: false,
+  S: false,
+  D: false
+}
+document.addEventListener('keydown', event => {
+  const key = event.key.toUpperCase();
+  if (Object.keys(Pressed).includes(key)) {
+    Pressed[key] = true;
+  }
+  if (event.code === 'Space') {
+    const bulletGeom = new THREE.BoxBufferGeometry(0.2, 0.2, 0.2);
+    const bulletMaterial = new THREE.MeshBasicMaterial({ color: '#ffffff' });
+    const bullet = new THREE.Mesh(bulletGeom, bulletMaterial);
+    const position = new THREE.Vector3();
+    const quaternion = new THREE.Quaternion();
+    front.getWorldPosition(position);
+    front.getWorldQuaternion(quaternion);
+    bullet.position.copy(position);
+    bullet.quaternion.copy(quaternion);
+    scene.add(bullet);
+    bullets.push(bullet);
+    setTimeout(() => {
+      bullet.geometry.dispose();
+      bullet.material.dispose();
+      scene.remove(bullet);
+    }, 500);
+  }
+});
+document.addEventListener('keyup', event => {
+  const key = event.key.toUpperCase();
+  if (Object.keys(Pressed).includes(key)) {
+    Pressed[key] = false;
+  }
+});
+
+const movementSpeed = 0.25;
+const rotationSpeed = 0.025;
+
 // render
 renderer.setAnimationLoop(time => {
-  mesh.rotation.y = time * 0.001;
+  for (const bullet of bullets) {
+    bullet.translateY(1)
+  }
+  if (Pressed.W) {
+    mesh.translateY(movementSpeed);
+  }
+  else if (Pressed.S) {
+    mesh.translateY(-movementSpeed);
+  }
+  if (Pressed.A) {
+    mesh.rotation.z += Math.PI * rotationSpeed;
+  }
+  else if (Pressed.D) {
+    mesh.rotation.z += -Math.PI * rotationSpeed;
+  }
   renderer.render(scene, camera);
 });
 

e0fbc93235656a473e19491f5f47aece23b0c182
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2022-04-21 11:45:31 +0800

    Add: WASD movement sample

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const camera = new THREE.PerspectiveCamera(
   75,
   canvasWidth / canvasHeight
 );
-camera.position.z = 3;
+camera.position.z = 10;
 
 // mesh
 const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
@@ -31,9 +31,41 @@ const material = new THREE.MeshNormalMaterial();
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+const InputHold = {
+  W: false,
+  A: false,
+  S: false,
+  D: false
+};
+document.addEventListener('keydown', event => {
+  const key = event.key.toUpperCase();
+  if (Object.keys(InputHold).includes(key)) {
+    InputHold[key] = true;
+  }
+});
+document.addEventListener('keyup', event => {
+  const key = event.key.toUpperCase();
+  if (Object.keys(InputHold).includes(key)) {
+    InputHold[key] = false;
+  }
+});
+
+const speed = 0.25;
+
 // render
 renderer.setAnimationLoop(time => {
-  mesh.rotation.y = time * 0.001;
+  if (InputHold.W) {
+    mesh.translateY(speed);
+  }
+  else if (InputHold.S) {
+    mesh.translateY(-speed);
+  }
+  if (InputHold.A) {
+    mesh.translateX(-speed);
+  }
+  else if (InputHold.D) {
+    mesh.translateX(speed);
+  }
   renderer.render(scene, camera);
 });
 

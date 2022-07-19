97d3bc0ef271ba1deec131297ceea81962634685
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2022-07-19 12:28:36 +0800

    Add: grid material experiment 1

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ import * as THREE from 'three';
 import GSAP from 'gsap';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GridMaterial, MorphedGridMaterial } from './src/materials';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass';
+import { BokehPass } from 'three/examples/jsm/postprocessing/BokehPass';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
 
 
 const canvas = document.getElementById('app');
@@ -31,21 +37,32 @@ camera.position.z = 25;
 class MorphedSphere {
   constructor() {
     const geometry = new THREE.IcosahedronBufferGeometry(10, 32);
-    const material = MorphedGridMaterial;
+    const material = GridMaterial;
     this.mouse = new THREE.Vector2();
 
     this.mesh = new THREE.Mesh(geometry, material);
     scene.add(this.mesh);
 
-    window.addEventListener('mousemove', event => {
-      this.mouse.x = (event.clientX / window.innerWidth).toFixed(2) * 4
-      this.mouse.y = (event.clientY / window.innerHeight).toFixed(2) * 2;
+    // window.addEventListener('mousemove', event => {
+    //   this.mouse.x = (event.clientX / window.innerWidth).toFixed(2) * 4
+    //   this.mouse.y = (event.clientY / window.innerHeight).toFixed(2) * 2;
 
-      GSAP.to(this.mesh.material.uniforms.uFrequency, { value: this.mouse.x });
-      GSAP.to(this.mesh.material.uniforms.uAmplitude, { value: this.mouse.x });
-      GSAP.to(this.mesh.material.uniforms.uDensity, { value: this.mouse.y });
-      GSAP.to(this.mesh.material.uniforms.uStrength, { value: this.mouse.y });
-    });
+    //   GSAP.to(this.mesh.material.uniforms.uFrequency, { value: this.mouse.x });
+    //   GSAP.to(this.mesh.material.uniforms.uAmplitude, { value: this.mouse.x });
+    //   GSAP.to(this.mesh.material.uniforms.uDensity, { value: this.mouse.y });
+    //   GSAP.to(this.mesh.material.uniforms.uStrength, { value: this.mouse.y });
+    // });
+  }
+}
+class Cube {
+  constructor() {
+    const geometry = new THREE.BoxBufferGeometry(10, 10, 10);
+    const material = GridMaterial;
+    this.mouse = new THREE.Vector2();
+
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.mesh.position.x = 20;
+    scene.add(this.mesh);
   }
 }
 
@@ -62,15 +79,86 @@ class Ground {
 
 new MorphedSphere();
 new Ground();
+new Cube();
 
 // controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
+
+// postprocessing
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight), 0.4, 0, 0
+);
+
+const renderPass = new RenderPass(scene, camera);
+
+const smaaPass = new SMAAPass(
+  window.innerWidth * renderer.getPixelRatio(),
+  window.innerHeight * renderer.getPixelRatio()
+);
+smaaPass.renderToScreen = true;
+
+const bokehPass = new BokehPass(scene, camera, {
+  focus: 10,
+  aperture: 10.0 * 0.00001,
+  maxblur: 0.01,
+  width: window.innerWidth,
+  height: window.innerHeight
+});
+
+const bloomComposer = new EffectComposer(renderer);
+bloomComposer.renderToScreen = false;
+bloomComposer.addPass(renderPass);
+bloomComposer.addPass(bloomPass);
+
+const finalPass = new ShaderPass(
+  new THREE.ShaderMaterial({
+    uniforms: {
+      baseTexture: { value: null },
+      bloomTexture: { value: bloomComposer.renderTarget2.texture }
+    },
+    vertexShader: `
+      varying vec2 vUv;
+      void main()
+      {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      }
+    `,
+    fragmentShader: `
+      uniform sampler2D baseTexture;
+      uniform sampler2D bloomTexture;
+      varying vec2 vUv;
+      void main()
+      {
+        gl_FragColor = ( texture2D( baseTexture, vUv ) + vec4( 1.0 ) * texture2D( bloomTexture, vUv ) );
+      }
+    `,
+    defines: {}
+  }), "baseTexture"
+);
+finalPass.needsSwap = true;
+
+const effectComposer = new EffectComposer(renderer);
+effectComposer.addPass(renderPass);
+effectComposer.addPass(smaaPass);
+effectComposer.addPass(bloomPass);
+// effectComposer.addPass(bokehPass);
+// effectComposer.addPass(finalPass);
+
 // render
 renderer.setAnimationLoop(time => {
   controls.update();
-  renderer.render(scene, camera);
+  // renderer.render(scene, camera);
+  try {
+    renderer.clear();
+    bloomComposer.render();
+    effectComposer.render();
+  }
+  catch (error) {
+    renderer.render(scene, camera);
+  }
 });
 
 // handle responsiveness
--- src/materials.js
+++ src/materials.js
@@ -26,6 +26,8 @@ const gridShaderSnippets = {
     out vec3 vNorm;
     out vec3 vScale;
     out vec3 vColor;
+
+    uniform float uScaleMultiplier;
   `,
   main: `
     vec4 mvPosition = vec4( position, 1.0 );
@@ -39,7 +41,7 @@ const gridShaderSnippets = {
     float scaleX = sqrt(cm[0][0]*cm[0][0] + cm[0][1]*cm[0][1] + cm[0][2]*cm[0][2]);
     float scaleY = sqrt(cm[1][0]*cm[1][0] + cm[1][1]*cm[1][1] + cm[1][2]*cm[1][2]);
     float scaleZ = sqrt(cm[2][0]*cm[2][0] + cm[2][1]*cm[2][1] + cm[2][2]*cm[2][2]);
-    vScale = vec3(scaleX, scaleY, scaleZ);
+    vScale = vec3(scaleX, scaleY, scaleZ) * uScaleMultiplier;
 
     vWorldPosition = (mvPosition).xyz;
     vNorm = normal;
@@ -72,7 +74,7 @@ const gridShaderSnippets = {
       vec3 coord = vWorldPosition / scale * vScale;
 
       vec3 grid = abs(fract(coord - 0.5) - 0.5) / fwidth(coord);
-      float line = min(min(grid.x, grid.y), grid.z);
+      float line = min(min(grid.x, grid.y), grid.z) * 0.25;
 
       return  min(line, 1.0);
 
@@ -81,13 +83,13 @@ const gridShaderSnippets = {
     }
 
     void main() {
-        float d = 1.0 - min(distance(cameraPosition.xz, vWorldPosition.xz) / 200., 1.0);
+        // float d = 1.0 - min(distance(cameraPosition.xz, vWorldPosition.xz) / 200., 1.0);
         float g1 = grid_tri_mapping(uSize1);
 
         vec3 col = uColor.rgb;
         vec3 diffuse = uLineColor;
 
-        pc_FragColor = vec4(mix(diffuse, col, g1 * pow(d, 3.0)), 1.);
+        pc_FragColor = vec4(mix(diffuse, col, g1), 1.);
 
         pc_FragColor.rgb = mix(pc_FragColor.rgb, vec3(0.), saturate(vNorm.g) * .05);
         pc_FragColor.a = 1.;
@@ -97,9 +99,10 @@ const gridShaderSnippets = {
 
 
 const gridUniforms = {
-  uSize1: { value: 10 },
-  uColor: { value: new THREE.Color('#000000') },
-  uLineColor: { value: new THREE.Color('#ffffff') },
+  uSize1: { value: 1 },
+  uColor: { value: new THREE.Color('#050505') },
+  uLineColor: { value: new THREE.Color('#0000ff') },
+  uScaleMultiplier: { value: 0.1 }
 };
 
 const morphUniforms = {

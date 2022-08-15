735bf64fe5bcb394e0670f0796ecdcfd3f734c4a
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2022-08-15 20:26:41 +0800

    Add: bubble 3

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 import * as THREE from 'three';
-
+import { fragmentShader, vertexShader } from './src/shaders';
 
 const canvas = document.getElementById('app');
 
@@ -23,17 +23,32 @@ const camera = new THREE.PerspectiveCamera(
   75,
   canvasWidth / canvasHeight
 );
-camera.position.z = 3;
+camera.position.z = 20;
 
 // mesh
-const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
-const material = new THREE.MeshNormalMaterial();
+const geometry = new THREE.IcosahedronBufferGeometry(10, 64);
+const material = new THREE.ShaderMaterial({
+  uniforms: {
+    uFrequency: { value: 2.4 },
+    uAmplitude: { value: 0.4 },
+    uDensity: { value: 0.8 },
+    uStrength: { value: 1.8 },
+    uDeepPurple: { value: 1 },
+    uOpacity: { value: 0.1 },
+    uTime: { value: 0 },
+    uDepthColor: { value: new THREE.Color('#0458FF') },
+    uSurfaceColor: { value: new THREE.Color('#F20089') },
+  },
+  vertexShader,
+  fragmentShader,
+  transparent: true,
+});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 // render
 renderer.setAnimationLoop(time => {
-  mesh.rotation.y = time * 0.001;
+  material.uniforms.uTime.value = time * 0.0002;
   renderer.render(scene, camera);
 });
 

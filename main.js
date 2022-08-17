93a489c2af3fd2791a11fdd17da9dc6b5d4d9c78
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2022-08-17 14:00:27 +0800

    Add: rubiks cube

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import './style.css';
 import * as THREE from 'three';
+import Stats from 'stats-js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { createRubiksCube } from './src/rubiks-cube';
 
 
 const canvas = document.getElementById('app');
@@ -23,18 +26,60 @@ const camera = new THREE.PerspectiveCamera(
   75,
   canvasWidth / canvasHeight
 );
-camera.position.z = 3;
+camera.position.z = 100;
 
 // mesh
-const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
-const material = new THREE.MeshNormalMaterial();
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const rubiksCube = createRubiksCube();
+scene.add(rubiksCube.object);
+
+// controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const lightGroup = new THREE.Group();
+scene.add(lightGroup);
+
+const light1 = new THREE.PointLight('#ffffff', 1, 0);
+light1.position.set(0, 200, 200);
+lightGroup.add(light1);
+
+const light2 = new THREE.PointLight('#ffffff', 0.1, 0);
+light2.position.set(-100, 200, 100);
+lightGroup.add(light2);
+
+const light3 = new THREE.PointLight('#ffffff', 1, 0);
+light3.position.set(-100, -200, -100);
+lightGroup.add(light3);
+
+const light4 = new THREE.PointLight('#2472ca', 3, 0);
+light4.position.set(0, 200, 0);
+lightGroup.add(light4);
+
+const light5 = new THREE.PointLight('#2472ca', 3, 0);
+light4.position.set(0, 0, 200);
+scene.add(light5);
+
+const directionalLight = new THREE.DirectionalLight('#fffff');
+scene.add(directionalLight);
+
+const ambientLight = new THREE.AmbientLight('#000000');
+lightGroup.add(ambientLight);
+
+lightGroup.lookAt(new THREE.Vector3(40, -30, 0));
+
+// lightGroup.rotation.y = Math.PI * 0.5;
+
+const stats = new Stats();
+stats.showPanel(0);
+document.body.appendChild(stats.dom);
 
 // render
 renderer.setAnimationLoop(time => {
-  mesh.rotation.y = time * 0.001;
+  stats.begin();
+  controls.update();
+  rubiksCube.update(time);
   renderer.render(scene, camera);
+  stats.end();
 });
 
 // handle responsiveness
---
+++ src/rubiks-cube.js
@@ -0,0 +1,314 @@
+import anime from 'animejs';
+import * as THREE from 'three';
+import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry';
+
+const createCubePiece = (size) => {
+  const geometry = new RoundedBoxGeometry(size, size, size, 6, 0.75);
+  const material = new THREE.MeshPhongMaterial({
+    color: '#2170c9',
+    specular: '#ff5900',
+    shininess: 30,
+    opacity: 1,
+  });
+
+  material.onBeforeCompile = shader => {
+    shader.uniforms.uLowColor = { value: new THREE.Color('#0458FF') };
+    shader.uniforms.uHighColor = { value: new THREE.Color('#7209B7') };
+
+    shader.vertexShader = shader.vertexShader.replace('void main() {', `
+      varying vec3 vPositionW;
+      varying vec3 vNormalW;
+
+      void main() {
+        vPositionW = vec3( vec4( position, 1.0 ) * modelMatrix);
+        vNormalW = normalize( vec3( vec4( normal, 0.0 ) * modelMatrix ) );
+    `);
+    shader.fragmentShader = shader.fragmentShader.replace('void main() {', `
+      uniform vec3 uLowColor;
+      uniform vec3 uHighColor;
+
+      varying vec3 vPositionW;
+      varying vec3 vNormalW;
+
+      void main() {
+    `);
+    shader.fragmentShader = shader.fragmentShader.replace('vec4 diffuseColor = vec4( diffuse, opacity );', `
+      vec3 viewDirectionW = normalize(cameraPosition - vPositionW);
+      float fresnelTerm = dot(viewDirectionW, vNormalW);
+      fresnelTerm = clamp(1.0 - fresnelTerm, 0., 1.);
+
+      vec4 diffuseColor = vec4( mix(uLowColor, uHighColor, fresnelTerm), opacity );
+      // vec4 diffuseColor = vec4( diffuse, opacity );
+    `);
+  }
+
+  const mesh = new THREE.Mesh(geometry, material);
+  return mesh;
+}
+
+export const createRubiksCube = () => {
+  const object = new THREE.Group();
+
+  const gap = 2;
+  const size = 15;
+  const cubes = [];
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      for (let k = -1; k <= 1; k++) {
+        const mesh = createCubePiece(size);
+        const x = (i * size);
+        const y = (j * size);
+        const z = (k * size);
+        mesh.name = `x: ${x} | y: ${y} | z: ${z}`;
+        cubes.push(mesh);
+        object.add(mesh);
+        mesh.position.set(x, y, z);
+      }
+    }
+  }
+
+  const range = gap;
+
+  const xLowAxle = new THREE.Group();
+  xLowAxle.position.set(-range, 0, 0);
+  object.add(xLowAxle);
+
+  const xMidAxle = new THREE.Group();
+  xMidAxle.position.set(0, 0, 0);
+  object.add(xMidAxle);
+
+  const xHighAxle = new THREE.Group();
+  xHighAxle.position.set(range, 0, 0);
+  object.add(xHighAxle);
+
+  const yLowAxle = new THREE.Group();
+  yLowAxle.position.set(0, -range, 0);
+  object.add(yLowAxle);
+
+  const yMidAxle = new THREE.Group();
+  yMidAxle.position.set(0, 0, 0);
+  object.add(yMidAxle);
+
+  const yHighAxle = new THREE.Group();
+  yHighAxle.position.set(0, range, 0);
+  object.add(yHighAxle);
+
+  const zLowAxle = new THREE.Group();
+  zLowAxle.position.set(0, 0, -range);
+  object.add(zLowAxle);
+
+  const zMidAxle = new THREE.Group();
+  zMidAxle.position.set(0, 0, 0);
+  object.add(zMidAxle);
+
+  const zHighAxle = new THREE.Group();
+  zHighAxle.position.set(0, 0, range);
+  object.add(zHighAxle);
+
+  const worldPosition = new THREE.Vector3();
+
+  const duration = 500;
+  const easing = 'easeOutCubic';
+  const targetRotation = Math.PI * 0.5;
+  const moveLowX = () => {
+    const targets = cubes.filter(cube => {
+      cube.getWorldPosition(worldPosition);
+      return cube.position.x < -14;
+    });
+    for (const piece of targets) {
+      xLowAxle.attach(piece);
+    }
+    anime({
+      targets: xLowAxle.rotation,
+      x: `+=${targetRotation}`,
+      easing,
+      duration,
+      complete: (anim) => {
+        moveRandom();
+      }
+    });
+  }
+
+  const moveMidX = () => {
+    const targets = cubes.filter(cube => {
+      cube.getWorldPosition(worldPosition);
+      return cube.position.x <= 1 && cube.position.x >= -1;
+    });
+    for (const piece of targets) {
+      xMidAxle.attach(piece);
+    }
+    anime({
+      targets: xMidAxle.rotation,
+      x: `+=${targetRotation}`,
+      easing,
+      duration,
+      complete: (anim) => {
+        moveRandom();
+      }
+    });
+  }
+
+  const moveHighX = () => {
+    const targets = cubes.filter(cube => {
+      cube.getWorldPosition(worldPosition);
+      return cube.position.x > 14;
+    });
+    for (const piece of targets) {
+      xHighAxle.attach(piece);
+    }
+    anime({
+      targets: xHighAxle.rotation,
+      x: `+=${targetRotation}`,
+      easing,
+      duration,
+      complete: (anim) => {
+        moveRandom();
+      }
+    });
+  }
+
+  const moveLowY = () => {
+    const targets = cubes.filter(cube => {
+      cube.getWorldPosition(worldPosition);
+      return cube.position.y < -14;
+    });
+    for (const piece of targets) {
+      yLowAxle.attach(piece);
+    }
+    anime({
+      targets: yLowAxle.rotation,
+      y: `+=${targetRotation}`,
+      easing,
+      duration,
+      complete: (anim) => {
+        moveRandom();
+      }
+    });
+  }
+
+  const moveMidY = () => {
+    const targets = cubes.filter(cube => {
+      cube.getWorldPosition(worldPosition);
+      return cube.position.y < 1 && cube.position.y > -1;
+    });
+    for (const piece of targets) {
+      yMidAxle.attach(piece);
+    }
+    anime({
+      targets: yMidAxle.rotation,
+      y: `+=${targetRotation}`,
+      easing,
+      duration,
+      complete: (anim) => {
+        moveRandom();
+      }
+    });
+  }
+
+  const moveHighY = () => {
+    const targets = cubes.filter(cube => {
+      cube.getWorldPosition(worldPosition);
+      return cube.position.y > 14;
+    });
+    for (const piece of targets) {
+      yHighAxle.attach(piece);
+    }
+    anime({
+      targets: yHighAxle.rotation,
+      y: `+=${targetRotation}`,
+      easing,
+      duration,
+      complete: (anim) => {
+        moveRandom();
+      }
+    });
+  }
+
+  const moveLowZ = () => {
+    const targets = cubes.filter(cube => {
+      cube.getWorldPosition(worldPosition);
+      return cube.position.z < -14;
+    });
+    for (const piece of targets) {
+      zLowAxle.attach(piece);
+    }
+    anime({
+      targets: zLowAxle.rotation,
+      z: `+=${targetRotation}`,
+      easing,
+      duration,
+      complete: (anim) => {
+        moveRandom();
+      }
+    });
+  }
+
+  const moveMidZ = () => {
+    const targets = cubes.filter(cube => {
+      cube.getWorldPosition(worldPosition);
+      return cube.position.z < 1 && cube.position.z > -1;
+    });
+    for (const piece of targets) {
+      zLowAxle.attach(piece);
+    }
+    anime({
+      targets: zLowAxle.rotation,
+      z: `+=${targetRotation}`,
+      easing,
+      duration,
+      complete: (anim) => {
+        moveRandom();
+      }
+    });
+  }
+
+  const moveHighZ = () => {
+    const targets = cubes.filter(cube => {
+      cube.getWorldPosition(worldPosition);
+      return cube.position.z > 14;
+    });
+    for (const piece of targets) {
+      zHighAxle.attach(piece);
+    }
+    anime({
+      targets: zHighAxle.rotation,
+      z: `+=${targetRotation}`,
+      easing,
+      duration,
+      complete: (anim) => {
+        moveRandom();
+      }
+    });
+  }
+
+  const moveFunctions = [
+    moveLowX,
+    moveMidX,
+    moveHighX,
+    moveLowY,
+    moveMidY,
+    moveHighY,
+    moveLowZ,
+    moveMidZ,
+    moveHighZ,
+  ];
+
+  const moveRandom = () => {
+    for (const cube of cubes) {
+      object.attach(cube);
+    }
+    const move = moveFunctions[Math.floor(Math.random()*moveFunctions.length)];
+    move();
+  }
+
+  moveRandom();
+
+  object.lookAt(new THREE.Vector3(40, -30, 50));
+  const update = (time) => {
+    object.rotation.y = time * 0.0005;
+  }
+  return {
+    object,
+    update
+  };
+}

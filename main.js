6415a30dab25245a94ffb81e2925e3ee31016fd9
Ray Lawrence Henri Sison <raylawrencehenrisison@gmail.com>
2024-10-18 22:44:52 +0800

    Add: Galaxy

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 import * as THREE from 'three';
-
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const canvas = document.getElementById('app');
 
@@ -23,17 +23,65 @@ const camera = new THREE.PerspectiveCamera(
   75,
   canvasWidth / canvasHeight
 );
-camera.position.z = 3;
+camera.position.z = 10;
 
 // mesh
-const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
-const material = new THREE.MeshNormalMaterial();
-const mesh = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BufferGeometry();
+const count = 10000;
+const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
+const radius = 10;
+const branches = 3;
+const spin = 1;
+const spread = 5;
+
+const color1 = new THREE.Color('#ADD8E6');
+const color2 = new THREE.Color('#101820');
+
+for (let i = 0; i < count; i++) {
+  const i3 = i * 3;
+  const x = i3;
+  const y = i3 + 1;
+  const z = i3 + 2;
+
+  const localRadius = Math.random() * radius;
+  const branch = i % 3;
+  const branchAngle = branch / branches * Math.PI * 2;
+  const spinAngle = spin * localRadius;
+
+  const randomX = Math.pow(Math.random(), spread) * (Math.random() > 0.5 ? -1 : 1);
+  const randomY = Math.pow(Math.random(), spread) * (Math.random() > 0.5 ? -1 : 1);
+  const randomZ = Math.pow(Math.random(), spread) * (Math.random() > 0.5 ? -1 : 1);
+
+  positions[x] = Math.cos(branchAngle + spinAngle) * localRadius + randomX;
+  positions[y] = randomY;
+  positions[z] = Math.sin(branchAngle + spinAngle) * localRadius + randomZ;
+
+  const color = color1.clone();
+  color.lerp(color2, localRadius / radius);
+  colors[x] = color.r;
+  colors[y] = color.g;
+  colors[z] = color.b;
+}
+geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+const material = new THREE.PointsMaterial({
+  size: 0.1,
+  sizeAttenuation: true,
+  vertexColors: true,
+  blending: THREE.AdditiveBlending,
+});
+const mesh = new THREE.Points(geometry, material);
+mesh.rotation.x = Math.PI * 0.25;
 scene.add(mesh);
+
+const controls = new OrbitControls(camera, canvas);
 
 // render
 renderer.setAnimationLoop(time => {
-  mesh.rotation.y = time * 0.001;
+  controls.update();
+  mesh.rotation.y = time * 0.0001;
   renderer.render(scene, camera);
 });
 
